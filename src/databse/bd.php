2fccdc4321e804bc8439b92a0a4b7c215365e4ff
Vicente Rojas <virojas15@alumnos.utalca.cl>
2021-10-21 19:09:20 -0300

    DB Ready v2

--- src/databse/bd.php
+++ src/databse/bd.php
@@ -7,7 +7,7 @@ $contraseña = "tecweb";
 $usuario = "feryx";
 $nombre_base_de_datos = "meanoto";
 try {
-    return new PDO('mysql:host=179.56.165.81;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+    return new PDO('mysql:host=meanoto.ddns.net;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 } catch (Exception $e) {
     echo "Ocurrió algo con la base de datos: " . $e->getMessage();
 }
