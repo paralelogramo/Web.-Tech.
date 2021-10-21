4ca51d365e187089f26fd433d6ee3a907ce16dc8
Vicente Rojas <virojas15@alumnos.utalca.cl>
2021-10-21 19:05:14 -0300

    DB ready

--- src/databse/bd.php
+++ src/databse/bd.php
@@ -3,11 +3,11 @@ header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Headers: *");
 header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-$contraseña = "";
-$usuario = "root";
+$contraseña = "tecweb";
+$usuario = "feryx";
 $nombre_base_de_datos = "meanoto";
 try {
-    return new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+    return new PDO('mysql:host=179.56.165.81;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 } catch (Exception $e) {
     echo "Ocurrió algo con la base de datos: " . $e->getMessage();
 }
